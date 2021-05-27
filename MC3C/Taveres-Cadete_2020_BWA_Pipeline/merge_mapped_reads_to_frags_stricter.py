82ac2300bcd24108313adeeb84479270e46634f7
ehildebr <erica.hildebrand@umassmed.edu>
2021-05-27 12:22:21 -0400

    Update merge_mapped_reads_to_frags_stricter.py
    Added some comments to describe what this script is doing

--- MC3C/Taveres-Cadete_2020_BWA_Pipeline/merge_mapped_reads_to_frags_stricter.py
+++ MC3C/Taveres-Cadete_2020_BWA_Pipeline/merge_mapped_reads_to_frags_stricter.py
@@ -11,9 +11,9 @@ from collections import OrderedDict
 import numpy
 import pickle
 
-fragment_file_path = "/nl/umw_job_dekker/archive/cshare/genome/restrictionFragments/hg19/hg19__GATC.txt"
+fragment_file_path = "/nl/umw_job_dekker/users/eh37w/Topo-Inhib/MC3C/reference/hg19__GATC.txt" #changed to new path
 
-bam_reader = HTSeq.BAM_Reader( sys.argv[1] )
+bam_reader = HTSeq.BAM_Reader( sys.argv[1] ) #uses HTSeq.BAM_Reader to read in alignments - digested MC-3C to undigested genome
 #
 #alignment_read_names = []
 #
@@ -50,43 +50,45 @@ bam_reader = HTSeq.BAM_Reader( sys.argv[1] )
 
 
 
+#making dict of all of the DpnII digested fragments in genome, will compare the alignments to these fragments. 
 
 fragment_ranges = {}
-for i in range(1, 23):
+for i in range(1, 23): #first adding chromosomes - this will depend on the genome that is used
     fragment_ranges[ "chr"+str(i)] = []
 
 fragment_ranges["chrX"] = []
 fragment_ranges["chrM"] = []
 fragment_ranges["chrY"] = []
 
-for fragment_range in HTSeq.BED_Reader( fragment_file_path ):
+for fragment_range in HTSeq.BED_Reader( fragment_file_path ): #next adding start and end of each fragment iv stands for interval - part of bed reader output (https://htseq.readthedocs.io/en/master/otherparsers.html#bed-reader)
 
-    fragment_range.iv.start -= 1
+    fragment_range.iv.start -= 1 #subtract 1 from the start and the end of each interval - probably due to 1 vs 0 based intervals? Python vs R?
     fragment_range.iv.end -= 1
 
-    fragment_ranges[fragment_range.iv.chrom].append( fragment_range )
+    fragment_ranges[fragment_range.iv.chrom].append( fragment_range ) #Add fragment range to correct chromosome 
 
  
-frag_pacbio_dict = {}
+frag_pacbio_dict = {} #initialize dict for fragments that match MC-3C alignments
 
-for a in bam_reader:
-    read_id = a.read.name.split("/")[1]
-    if len(a.read.name.split("/")) == 4:
-        digest_id = a.read.name.split("/")[3]
+for a in bam_reader: #from earlier HTSeq_BAMReader call - this is the alignments
+    read_id = a.read.name.split("/")[1] #BAM read.name contains moviename/holenumber/ccs/subreadnumber (https://pacbiofileformats.readthedocs.io/en/5.0/BAM.html)
+    #holenumber should be unique for each ccs in a given movie - so this is used as the read_id
+    if len(a.read.name.split("/")) == 4: #determine if there are subreads - if not, will only be length 3
+        digest_id = a.read.name.split("/")[3] #digest_id number from bam read.name of this alignment - where within css it came from (order)
     else:
         digest_id = "0"
-    if (read_id in frag_pacbio_dict) == False:
+    if (read_id in frag_pacbio_dict) == False: #check if this read_id has already been added - if not, add it. 
         frag_pacbio_dict[read_id] = OrderedDict()
-    if (digest_id in frag_pacbio_dict[read_id]) == False:
+    if (digest_id in frag_pacbio_dict[read_id]) == False: #check if this digest id has already been added, if not, add it
         frag_pacbio_dict[read_id][digest_id] = []
-    if (a.aligned) & (a.aQual == 60):
-        for frag in fragment_ranges[a.iv.chrom]:
-            if a.iv.overlaps(frag.iv):
+    if (a.aligned) & (a.aQual == 60): #only add an aligned fragment if alignment is high quality
+        for frag in fragment_ranges[a.iv.chrom]: #Go through all genome fragments in the matching chromosome to see if they overlap with alignment
+            if a.iv.overlaps(frag.iv): #if they overlap, calculate how much of fragment overlaps with alignment
                 overlap_length = float((min([a.iv.end, frag.iv.end]) - max([a.iv.start, frag.iv.start]))) / (frag.iv.end - frag.iv.start)
-                if overlap_length >= 0.8:
+                if overlap_length >= 0.8: #if it is over 80% overlap, add this fragment to the frag_pacbio_dict as the alignment
                     frag_pacbio_dict[read_id][digest_id].append((frag, a.iv.strand))
 
-pickle.dump( frag_pacbio_dict, open(sys.argv[1].replace(".bam", "_stricter.pickle"), "wb")) 
+pickle.dump( frag_pacbio_dict, open(sys.argv[1].replace(".bam", "_stricter.pickle"), "wb")) #save dict
 
 
 #proportion_digests = []
