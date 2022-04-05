62c4fbf03d593ca978cd7462a73142c1c3303884
ehildebr <erica.hildebrand@umassmed.edu>
2022-04-05 09:58:04 -0400

    Delete Older_Scripts directory

--- Older_Scripts/Taveres-Cadete_Pipeline/launch_reduce_frag_mappings_exp4_39_restofsamples.sh
+++
@@ -1,9 +0,0 @@
-INPUT_DIR=/nl/umw_job_dekker/users/eh37w/Topo-Inhib/MC3C/alignments/exp4-39
-
-SCRIPT_DIR=/nl/umw_job_dekker/users/eh37w/Topo-Inhib/MC3C/scripts
-
-
-for f in $INPUT_DIR/*sorted_stricter.fragments.txt; do
-	echo "$f"
-	bsub -q short -W 4:00 -e /home/eh37w/lsf_jobs/LSB_%J.err -o /home/eh37w/lsf_jobs/LSB_%J.log -n 8 -R "select[ib]" -R "rusage[mem=2000]" -R "span[hosts=1]" -N -u erica.hildebrand@umassmed.edu "Rscript $SCRIPT_DIR/reduce_frag_mappings.R $f"
-done
